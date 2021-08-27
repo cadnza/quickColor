c6563e6a874d48f736966f4793eeda2d1bac136f
Jon Dayley <jonathandayley@gmail.com>
2021-08-27 11:01:22 -0600

    Add validation to quickColor

--- R/quickColor.R
+++ R/quickColor.R
@@ -2,6 +2,12 @@
 
 quickColor <- function(txt,fg=NA,bg=NA,bold=FALSE){
 
+	# Validate colors
+	msgColor <- "Please supply valid Xterm color numbers."
+	for(clrInst in c(fg,bg))
+		if(!clrInst%in%c(NA,clrs$xterm))
+			stop(msgColor)
+
 	# Apply styles
 	final <- txt
 	if(!is.na(fg))
