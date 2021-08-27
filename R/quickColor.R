5efff92864654d6752ff5fcfed2a6bbfb9e5fca1
Jon Dayley <jonathandayley@gmail.com>
2021-08-27 10:58:37 -0600

    Clean up quickColor

--- R/quickColor.R
+++ R/quickColor.R
@@ -1,12 +1,17 @@
 # Applies xterm color
 
-quickColor <- function(x,fg=NA,bg=NA,bold=FALSE){
-	final <- x
+quickColor <- function(txt,fg=NA,bg=NA,bold=FALSE){
+
+	# Apply styles
+	final <- txt
 	if(!is.na(fg))
 		final <- crayon::make_style(clrs$hex[clrs$xterm==fg])(final)
 	if(!is.na(bg))
 		final <- crayon::make_style(clrs$hex[clrs$xterm==bg],bg=TRUE)(final)
 	if(!is.na(bold))
 		final <- crayon::bold(final)
+
+	# Return
 	return(final)
+
 }
