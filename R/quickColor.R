c7107d40a491ef73e0cab78ed8fbb3eb41f6f7e2
Jon Dayley <jonathandayley@gmail.com>
2021-08-27 10:00:10 -0600

    Start quickColor function

---
+++ R/quickColor.R
@@ -0,0 +1,10 @@
+quickColor <- function(x,fg=NA,bg=NA,bold=FALSE){
+	final <- x
+	if(!is.na(fg))
+		final <- crayon::make_style(cd$hex[cd$xterm==fg])(final)
+	if(!is.na(bg))
+		final <- crayon::make_style(cd$hex[cd$xterm==bg],bg=TRUE)(final)
+	if(!is.na(bold))
+		final <- crayon::bold(final)
+	return(final)
+}
