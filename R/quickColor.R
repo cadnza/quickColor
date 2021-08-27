d3de3c095aa6c989bf6f40399256d58d30f1224c
Jon Dayley <jonathandayley@gmail.com>
2021-08-27 10:17:59 -0600

    Fix references to color data

--- R/quickColor.R
+++ R/quickColor.R
@@ -3,9 +3,9 @@
 quickColor <- function(x,fg=NA,bg=NA,bold=FALSE){
 	final <- x
 	if(!is.na(fg))
-		final <- crayon::make_style(cd$hex[cd$xterm==fg])(final)
+		final <- crayon::make_style(clrs$hex[clrs$xterm==fg])(final)
 	if(!is.na(bg))
-		final <- crayon::make_style(cd$hex[cd$xterm==bg],bg=TRUE)(final)
+		final <- crayon::make_style(clrs$hex[clrs$xterm==bg],bg=TRUE)(final)
 	if(!is.na(bold))
 		final <- crayon::bold(final)
 	return(final)
